ff39cd0ea7ced10fee61077eb89d868ec3458fcb
samuel-shyaka-dus <samyshyaka@gmail.com>
2022-06-07 15:33:44 +0200

    feat(auth): implement authentication

--- src/graphql/Link.ts
+++ src/graphql/Link.ts
@@ -1,5 +1,4 @@
 import { extendType, nonNull, objectType, stringArg } from "nexus";
-import { NexusGenObjects } from "../../nexus-typegen";
 
 export const Link = objectType({
     name: "Link",
@@ -7,21 +6,16 @@ export const Link = objectType({
         t.nonNull.int("id");
         t.nonNull.string("description");
         t.nonNull.string("url");
+        t.field("postedBy", {
+            type: "User",
+            resolve(parent, args, context) {
+                return context.prisma.link
+                    .findUnique({ where: { id: parent.id } })
+                    .postedBy();
+            }
+        })
     },
 });
-
-let links: NexusGenObjects["Link"][] = [
-    {
-        id: 1,
-        url: "www.howtographql.com",
-        description: "Fullstack tutorial for GraphQL",
-    },
-    {
-        id: 2,
-        url: "graphql.org",
-        description: "GraphQl official website",
-    },
-];
 
 export const LinkQuery = extendType({
     type: "Query",
@@ -29,7 +23,7 @@ export const LinkQuery = extendType({
         t.nonNull.list.nonNull.field("feed", {
             type: "Link",
             resolve(parent, args, context, info) {
-                return links;
+                return context.prisma.link.findMany();
             },
         });
     },
@@ -44,18 +38,23 @@ export const LinkMutation = extendType({
                 description: nonNull(stringArg()),
                 url: nonNull(stringArg()),
             },
-
             resolve(parent, args, context){
                 const { description, url } = args;
+                const { userId } = context;
 
-                let idCount = links.length + 1;
-                const link = {
-                    id: idCount,
-                    description: description,
-                    url: url,
-                };
-                links.push(link);
-                return link;
+                if(!userId) {
+                    throw new Error("Cannot post without logging in.")
+                }
+
+                const newLink = context.prisma.link.create ({
+                    data: {
+                        description,
+                        url,
+                        postedBy: { connect: { id: userId } },
+                    },
+                });
+                
+                return newLink;
             }
         })
     }
